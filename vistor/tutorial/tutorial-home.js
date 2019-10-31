816480d064ea02e1dcecd9225fe05ba1a413ce9a
balaji hambeere <balaji.hambeere@hotmail.com>
2019-10-31 19:02:32 +0530

    working on profile, category, tutorial page

--- vistor/tutorial/tutorial-home.js
+++ vistor/tutorial/tutorial-home.js
@@ -6,60 +6,70 @@ $(function () {
             title: "Angular Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/1.jpg",
+            updatedOn: "1 month ago"
         },
         {
             id: 103,
             title: "React Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/2.jpg",
+            updatedOn: "1 month ago"
         },
         {
             id: 104,
             title: "Expressjs Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/3.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 105,
             title: "Nodejs Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/4.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 106,
-            title: "JavaScript Tutorial",
+            title: "JavaScript Tutorial Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/5.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 107,
             title: "ES6 Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/6.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 101,
             title: "TypeScript Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/7.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 108,
             title: "BootStrap Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/8.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 109,
             title: "MongoDB Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/9.jpg",
+            updatedOn: " 1 month ago"
         },
         {
             id: 110,
             title: "Vuejs Tutorial",
             author: "Balaji Hambeere",
             imageUrl: "../../images/demopic/10.jpg",
+            updatedOn: " 1 month ago"
         }
     ];
 
@@ -84,7 +94,7 @@ $(function () {
         <img src="${item.imageUrl}" class="card-img-top" alt="${item.title}">
         <div class="card-body">
             <h5 class="card-title">${item.title}</h5>
-            <p class="card-text"><small class="text-muted">Author, ${item.author}</small></p>
+            <p class="card-text"><small class="text-muted">updated ${item.updatedOn}</small></p>
         </div>
     </div>`;
         return template;
